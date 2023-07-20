aa51485a134b26be3eed23876651505d1ef8745c
Dandara Estrela <dandara_estrela@hotmail.com>
2023-07-20 11:39:16 -0300

    feat: adding students register page

--- src/pages/StudentsRegister/StudentsRegister.styles.ts
+++ src/pages/StudentsRegister/StudentsRegister.styles.ts
@@ -1,1 +1,19 @@
 import styled from "styled-components";
+
+export const Wrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 24px 40px;
+  gap: 12px;
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 12px 20px;
+  gap: 8px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 4px;
+`;
--- src/pages/StudentsRegister/StudentsRegister.tsx
+++ src/pages/StudentsRegister/StudentsRegister.tsx
@@ -1,3 +1,47 @@
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { Row } from "@/components/DefaultStyles/Row";
+import Button from "@/components/Button/Button";
+import TextInput from "@/components/TextInput/TextInput";
+
+import * as S from "./StudentsRegister.styles";
+
+export const schema = Yup.object({
+  user: Yup.string().required("Campo obrigatório"),
+  userName: Yup.string().required("Campo obrigatório"),
+  password: Yup.string().required("Campo obrigatório"),
+});
+
 export function StudentsRegister() {
-  return <h1>StudentsRegister</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { user: "", username: "", password: "" },
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <S.Wrapper onSubmit={handleSubmit(onSubmit)}>
+      Cadastrar novo aluno
+      <S.ContentWrapper>
+        <Row $gap={8}>
+          <TextInput label="Nome" />
+          <TextInput label="Username" />
+          <TextInput label="Senha" />
+        </Row>
+        <Row $justifyContent="center">
+          <Button type="submit" col={6}>
+            Cadastrar usuário
+          </Button>
+        </Row>
+      </S.ContentWrapper>
+    </S.Wrapper>
+  );
 }
